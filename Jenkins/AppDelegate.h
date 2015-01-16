4f94e7056303fe7d90c8b5d51f21f7d9b4dbe616
sl_yang <shengli1222@163.com>
2015-01-16 13:19:58 +0800

    init

---
+++ Jenkins/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jenkins
+//
+//  Created by Antony on 15/1/16.
+//  Copyright (c) 2015年 DAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
